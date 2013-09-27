cf73e43c83f51379804500485351d361a4d84314
Nicholas Angeli <nicangeli@gmail.com>
2013-09-27 12:47:05 +0100

    Initial Commit

---
+++ Weave/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weave
+//
+//  Created by Nicholas Angeli on 27/09/2013.
+//  Copyright (c) 2013 Nicholas Angeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
