d7ab52ae33f9315ddb8f8cc128c94d3e18b135c6
Nicholas Angeli <nicangeli@gmail.com>
2013-10-01 16:20:55 +0100

    likes page view controller

---
+++ Weave/LikesPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LikesPageViewController.h
+//  Weave
+//
+//  Created by Nicholas Angeli on 01/10/2013.
+//  Copyright (c) 2013 Nicholas Angeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LikesPageViewController : UITableViewController
+
+@end
