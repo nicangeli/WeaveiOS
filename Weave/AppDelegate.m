090b01341a3bc010824615a715d5e18911bad093
Nicholas Angeli <nicangeli@gmail.com>
2013-11-04 15:40:54 +0000

    basic fb login functionality

--- Weave/AppDelegate.m
+++ Weave/AppDelegate.m
@@ -21,6 +21,13 @@
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    // attempt to extract a token from the url
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:self.session];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
@@ -71,7 +78,13 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-
+    
+    [FBAppEvents activateApp];
+    
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+    
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     Mixpanel *mixpanel = [Mixpanel sharedInstance];
    
@@ -83,12 +96,18 @@
     
     [mixpanel track:@"App Opened" properties:@{@"timestamp": dateString}];
     [mixpanel.people increment:@"played" by:[NSNumber numberWithInt:1]];
+    
+    // FBSample logic
+    // We need to properly handle activation of the application with regards to SSO
+    //  (e.g., returning from iOS 6.0 authorization dialog or from fast app switching).
+    [FBAppCall handleDidBecomeActiveWithSession:self.session];
 }
-     
+
      
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [self.session close];
 }
 
 
---
+++ Weave/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Weave
+//
+//  Created by Nicholas Angeli on 04/11/2013.
+//  Copyright (c) 2013 Nicholas Angeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
